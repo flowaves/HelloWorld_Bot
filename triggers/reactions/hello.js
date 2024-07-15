df4bb30cc040476576e762a6d542884a4f5a6201
Neptendus <jlaloy@proton.me>
2024-07-15 15:30:47 +0200

    add PingPong emoji & personalize hello trigger

--- triggers/reactions/hello.js
+++ triggers/reactions/hello.js
@@ -11,13 +11,10 @@
  * @type {import('../../typings').TriggerCommand}
  */
 module.exports = {
-	name: ["your", "trigger", "words", "in", "array"],
+	name: ["Bonjour", "bonjour", "Hello", "hello", "Coucou", "coucou", "Salut", "salut"],
 
 	execute(message, args) {
 		// Put all your trigger code over here. This code will be executed when any of the element in the "name" array is found in the message content.
-
-		message.channel.send({
-			content: "Set this trigger response from `./triggers/reactions/hello.js`",
-		});
+		message.react('👋');
 	},
 };
