cd3329fee423c574f442490b65f0627ad4e88d47
Neptendus <jlaloy@proton.me>
2024-07-31 11:57:26 +0200

    botinfo command personalization

--- interactions/slash/misc/botinfo.js
+++ interactions/slash/misc/botinfo.js
@@ -1,4 +1,4 @@
-const { SlashCommandBuilder } = require('discord.js');
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -14,8 +14,8 @@ module.exports = {
             .setDescription(`Bot créé le ${client.user.createdAt.toLocaleDateString()}`)
             .addFields(
                 { name: 'ID', value: client.user.id, inline: true },
-                { name: 'Version', value: '1.0.0', inline: true }, // Mettre à jour la version du bot si nécessaire
-                { name: 'Développeur', value: 'TonNomDeDéveloppeur', inline: true } // Remplace TonNomDeDéveloppeur par ton nom ou pseudo de développeur
+                { name: 'Version', value: '1.1', inline: true }, // Mettre à jour la version du bot si nécessaire
+                { name: 'Développeur', value: `<@1167190271390396517>`, inline: true } // Remplace TonNomDeDéveloppeur par ton nom ou pseudo de développeur
             );
 
         await interaction.reply({ embeds: [botInfoEmbed] });
